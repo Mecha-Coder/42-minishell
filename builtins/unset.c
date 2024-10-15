c3139fa365b63b2cd564c9688ed6a7af7d873d67
Jek John Tan <jetan@u90z02s03.42kl.edu.my>
2024-10-15 17:19:39 +0800

    while loop

--- builtins/unset.c
+++ builtins/unset.c
@@ -6,23 +6,28 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 14:49:49 by jetan             #+#    #+#             */
-/*   Updated: 2024/10/15 15:28:20 by jetan            ###   ########.fr       */
+/*   Updated: 2024/10/15 16:52:01 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+void	remove_env_var()
+{
+	
+}
+
 int	valid_name(char *name)
 {
 	int	i;
 	
-	if (ft_strcmp(name[0], "$") == 0)
+	if (ft_strcmp(name[0], "$") == 0 || ft_strcmp(name[0], "0123456789") == 0)
 		return (0);
 	i = 0;
 	while (name[++i])
 	{
 		if (ft_strcmp(name[i], "=") == 0)
-			return (0);	
+			return (0);
 	}
 	return (1);
 }
@@ -34,13 +39,13 @@ void	builtin_unset(char **av, t_env **env)//haven't finish
 {
 	int	i;
 	
+	if (!av[1])
+		return;
 	i = -1;
 	while (av[++i])
 	{
-		if ()
-			
-		= ->next;
+		if (!valid_name)
+			return;
+		remove_env_var();
 	}
 }
-
-update_env()
