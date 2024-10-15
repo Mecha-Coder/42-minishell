5d24e3b40682eac3ca7084088374bb594f7ddeea
Jek John Tan <jetan@u90z02s03.42kl.edu.my>
2024-10-15 15:29:07 +0800

    add valid_name

--- builtins/unset.c
+++ builtins/unset.c
@@ -6,24 +6,41 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 14:49:49 by jetan             #+#    #+#             */
-/*   Updated: 2024/10/03 12:55:10 by jetan            ###   ########.fr       */
+/*   Updated: 2024/10/15 15:28:20 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	builtin_unset(char **av)//haven't finish
+int	valid_name(char *name)
 {
 	int	i;
-	int	j;
-	int	var_count = 0;//struct
 	
+	if (ft_strcmp(name[0], "$") == 0)
+		return (0);
 	i = 0;
+	while (name[++i])
+	{
+		if (ft_strcmp(name[i], "=") == 0)
+			return (0);	
+	}
+	return (1);
+}
+
+/* this function check the argument match with env variable
+ * 
+*/
+void	builtin_unset(char **av, t_env **env)//haven't finish
+{
+	int	i;
+	
+	i = -1;
 	while (av[++i])
 	{
-		j = -1;
-		while (j < var_count)
-		{
-		}
+		if ()
+			
+		= ->next;
 	}
 }
+
+update_env()
