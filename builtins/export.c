1a2e6a6009a79665bf06334d138a95e0eabb7d4b
Jek John Tan <jetan@u90z04s06.42kl.edu.my>
2024-10-18 14:38:49 +0800

    update valid name

--- builtins/export.c
+++ builtins/export.c
@@ -6,69 +6,57 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:03:34 by jetan             #+#    #+#             */
-/*   Updated: 2024/10/17 16:51:53 by jetan            ###   ########.fr       */
+/*   Updated: 2024/10/18 14:37:05 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	add_update_env(t_env *env,char *key, char *val)
-{
-	while (env)
-	{
-		if (ft_strcmp(env->key, key) == 0)
-		{
-			free(env->val);
-			env->val = ft_strdup(val);
-			return;
-		}
-		env = env->next;
-	}
-}
-
-/* check validate variable name */
-int	valid_name(char *name)
+int	valid_name(char *str)
 {
 	int	i;
-	
-	if (ft_strcmp(name[0], "0123456789") == 0)
+
+	if (!str || (!ft_isalpha(str[0])) && str[0] != '_')
 		return (0);
-	i = 0;
-	while (name[++i])
+	i = 0;	
+	while (str[++i])
 	{
-		if (name[i] < 101 && name[i] > 172 && name[i] < 60 && name[i] > 71)
+		if (!ft_isalnum(str[i]) && str[i] != '_')
 			return (0);
 	}
 	return (1);
 }
 
-// No arguments, print sorted environment
-// Iterate over arguments, validate, and update environment
-void	builtin_export(char **av, t_env *env)
+void	builtin_export(char **args, t_env *env)
 {
 	int	i;
-	char	*key;
-	char	*val;
 	
-	if (!av[1])
-		return;
-	i = 0;
-	while (av[++i])
+	if (!args[1])
 	{
-		if (valid_name(av[i]))
+		
+		return;
+	}
+	i = 0;
+	while (args[++i])
+	{
+		if (!valid_name(args[i]))
 		{
-			if (ft_strcmp(av[i], '='))
-				add_update_env(av[i], );
-			else
-				mark_export();
+			
 		}
-		// else
-			//error message
-	}	
+		if (ft_strchr(args[i], '='))
+			add_update_env(args[i], env)
+		
+	}
 }
 
 int main(int ac, char **av, char **env)
 {
-	(void)ac;
-	builtin_export(av, env);
+    t_shell data;
+    (void)ac;
+    (void)av;
+
+    create_env(&data, env);
+    builtin_env(&data);
+    destroy_env(&data);
+    return 0;
 }
