abb5746e5df2665b1320486f4f96d3fbcabd4ac7
Jek John Tan <jetan@u90z02s03.42kl.edu.my>
2024-10-22 23:05:15 +0800

    add exec_cmd

---
+++ execution/exec_cmd.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_cmd.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 22:36:26 by jetan             #+#    #+#             */
+/*   Updated: 2024/10/22 22:58:27 by jetan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	exec_cmd(t_tree *node, t_env *env)
+{
+	pid_t	pid;
+	
+	if (!node)
+		return;
+	if (is_builtin(node->cmd[0]))
+		execute_builtin(node, env);
+	else
+	{
+		pid = fork;
+		if (pid == 0)
+		{
+			execve(node->cmd[0], node->cmd, convert_env_array(env));
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid < 0)
+		{
+			perror("fork");
+		}
+		else
+		{
+			waitpid(pid, NULL, 0);
+		}
+	}
+}
