33504e7084ffa1ee0bc0b3eca30b24f608474725
Jek John Tan <jetan@u90z04s02.42kl.edu.my>
2024-10-27 17:51:03 +0800

    remove is_builtin

--- execution/exec_builtin.c
+++ execution/exec_builtin.c
@@ -6,47 +6,28 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 17:44:31 by jetan             #+#    #+#             */
-/*   Updated: 2024/10/24 14:04:06 by jetan            ###   ########.fr       */
+/*   Updated: 2024/10/24 14:11:15 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	is_builtin(char *cmd)
-{
-	if (!cmd)
-		return (0);
-	if (ft_strcmp(cmd, "echo") == 0)
-		return (1);
-	else if (ft_strcmp(cmd, "cd") == 0)
-		return (1);
-	else if (ft_strcmp(cmd, "pwd") == 0)
-			return (1);
-	else if (ft_strcmp(cmd, "export") == 0)
-			return (1);
-	else if (ft_strcmp(cmd, "unset") == 0)
-			return (1);
-	else if (ft_strcmp(cmd, "env") == 0)
-			return (1);
-	else if (ft_strcmp(cmd, "exit") == 0)
-			return (1);
-	return (0);
-}
-
 void	exec_builtins(t_tree *node, t_env *env)
 {
 	if (ft_strcmp(node->cmd[0], "echo") == 0)
-		builtin_echo(->cmd);
+		builtin_echo(node->cmd);
 	else if (ft_strcmp(node->cmd[0], "cd") == 0)
-		builtin_cd(->cmd);
+		builtin_cd(node->cmd, env);
 	else if (ft_strcmp(node->cmd[0], "pwd") == 0)
-		builtin_pwd(->cmd);
+		builtin_pwd(node->cmd);
 	else if (ft_strcmp(node->cmd[0], "export") == 0)
-		builtin_export(->cmd);
+		builtin_export(node->cmd);
 	else if (ft_strcmp(node->cmd[0], "unset") == 0)
-		builtin_unset(->cmd);
+		builtin_unset(node->cmd);
 	else if (ft_strcmp(node->cmd[0], "env") == 0)
 		builtin_env(env);
 	else if (ft_strcmp(node->cmd[0], "exit") == 0)
-		builtin_exit(->cmd);
+		builtin_exit(node->cmd);
+	else
+		search_path();
 }
