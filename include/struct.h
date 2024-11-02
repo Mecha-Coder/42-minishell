da7398a9e8123842a2a916ce7f53b9dc6efa302b
Jek John Tan <jetan@u90z04s02.42kl.edu.my>
2024-11-02 18:47:15 +0800

    update struct.h

--- include/struct.h
+++ include/struct.h
@@ -8,27 +8,42 @@ typedef struct s_env
     struct s_env *next;
 } t_env;
 
-typedef struct s_file
+typedef struct s_token
 {
-    int mode;
-    char file[100];
-    int fd;
-    struct s_file *next;
-} t_file;
+    int type;
+    int index;
+    char *content;
+    int malloc; // FALSE, Indicate if content in dynamic memory
+} t_token;
 
 typedef struct s_tree
 {
-    char **cmd;
-    char **out;
+    int type;
+    t_token *token;
+    struct s_tree *left;
+    struct s_tree *right;
+
+    // During creation memset these values
+    char *cmd[CMD_SIZE];    // NULL
+    int in[OPEN_FD_SIZE];   // Must be > 2
+    int out[OPEN_FD_SIZE]; // Must be > 2
+    int *pipe_in;
+    int *pipe_out;
 } t_tree;
 
 typedef struct s_shell
 {
-    int exit_code; // 1
-    int interactive_mode;
-    char *input;
     t_env *env;
 
+    // Need to reset
+    char *input;
+    char s[BUFFER_SIZE];
+    int s_len;
+    t_tree *tree;
+    t_token token[CMD_SIZE];
+    int token_len;
+    int exit_cmd_no;
+    int exit_status;
 } t_shell;
 
 #endif
