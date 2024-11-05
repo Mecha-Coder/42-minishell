035b0aa361d1c741a1a433104f18ea904371c788
Jason Selau Kumar <148664294+Mecha-Coder@users.noreply.github.com>
2024-11-05 23:21:31 +0800

    Fixed prompt

--- read_input/prompt.c
+++ read_input/prompt.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
-#define RESET  "\033[0m"
-#define RED    "\033[0;31m"
-#define GREEN  "\033[0;32m"
-#define BLUE   "\033[0;36m"
-#define YELLOW "\033[0;33m"
+#define RESET  "\001\e[0m\002"
+#define RED    "\001\e[0;31m\002"
+#define GREEN  "\001\e[0;32m\002"
+#define BLUE   "\\001\e[0;36m\002"
+#define YELLOW "\001\e[0;33m\002"
 
-
+/*
 int main()
 {
-    char s[] = "\033[0;36m\u25A0[999] \033[0;33mMinishell> \033[0m";
+    char s[] = "\001\e[0;31m\002@[999] \001\e[0;33m\002Minishell>\001\e[0m\002 ";
     int x = (unsigned char)-1;
     int len = 13;
 
-    if (x)
-        s[5] = '1';
-    else
-        s[5] = '6';
-    
     while (len > 10)
     {
         s[len--] = (x % 10) + '0';
         x /= 10;
     }
     printf("%s\n", s);
+}*/
+
+
+// 6 [11-13]
+int main()
+{
+    int i = -1;
+    char s[] = "\001\e[0;31m\002@[999] \001\e[0;33m\002Minishell>\001\e[0m\002 ";
+    while(s[++i])
+        printf("%d : %c\n", i, s[i]);
 }
+
+
+
+
