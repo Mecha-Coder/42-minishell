08d621eb24aa66c280c083c33630d975d9d3da39
Jek John Tan <jetan@u90z08s04.42kl.edu.my>
2024-11-05 15:45:08 +0800

    add error_ msg

--- utils/error_handling.c
+++ utils/error_handling.c
@@ -6,22 +6,32 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:02:40 by jetan             #+#    #+#             */
-/*   Updated: 2024/11/04 15:56:08 by jetan            ###   ########.fr       */
+/*   Updated: 2024/11/05 15:44:44 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-// void	error_msg_export(char *builtin, char *arg, char *msg)
-// {
-// 	ft_putstr_fd("minishell: ", 2);
-// 	ft_putstr_fd(builtin, 2);
-// 	ft_putstr_fd(": ", 2);
-// 	ft_putstr_fd("`arg'", 2);
-// 	ft_putstr_fd(": ", 2);
-// 	ft_putstr_fd(msg, 2);
-// 	ft_putstr_fd("\n", 2);
-// }
+void	error_msg(char *builtin, char *args)
+{
+	if (ft_strcmp(builtin, "cd") == 0)
+		error_msg1("cd", "Home not set");
+	if (ft_strcmp(builtin, "echo") == 0)
+		error_msg_export("export", args, "not a valid identifier");
+	if (ft_strcmp(builtin, "exit") == 0)
+		error_arg_msg("exit", args, "numeric argument required");
+}
+
+void	error_msg_export(char *builtin, char *arg, char *msg)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(builtin, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd("`arg'", 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(msg, 2);
+	ft_putstr_fd("\n", 2);
+}
 
 void	error_arg_msg(char *builtin, char *arg, char *msg)
 {
@@ -34,7 +44,7 @@ void	error_arg_msg(char *builtin, char *arg, char *msg)
 	ft_putstr_fd("\n", 2);
 }
 
-void	error_msg(char *builtin, char *msg)
+void	error_msg1(char *builtin, char *msg)
 {
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(builtin, 2);
