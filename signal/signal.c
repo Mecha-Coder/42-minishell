e8c972db2790dfec3fc5cf524114cf5a65bdf666
Jek John Tan <jetan@u90z02s03.42kl.edu.my>
2024-11-07 16:04:31 +0800

    add print newline in sigint_handler

--- signal/signal.c
+++ signal/signal.c
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:29:23 by jetan             #+#    #+#             */
-/*   Updated: 2024/11/07 15:26:37 by jetan            ###   ########.fr       */
+/*   Updated: 2024/11/07 16:03:56 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	sigint_handler(int signo)
 {
 	(void)signo;
+	printf("\n");
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
