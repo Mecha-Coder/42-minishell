9bd3615f0f31adb9dca19cc9c2262f246b6cde11
Jek John Tan <jetan@u91z06s02.42kl.edu.my>
2024-11-14 14:21:22 +0800

    fixed norminette

--- built_in/export.c
+++ built_in/export.c
@@ -12,10 +12,10 @@
 
 #include "../include/minishell.h"
 
-static int process_var(char *s, t_env *env, int *track);
-static void print_sorted_env(t_env *env);
-static char **env_key_list(t_env *env);
-static void sort_list(char **list);
+static int	process_var(char *s, t_env *env, int *track);
+static void	print_sorted_env(t_env *env);
+static char	**env_key_list(t_env *env);
+static void	sort_list(char **list);
 
 /* Test
 int main()
@@ -91,95 +91,100 @@ Description: Update or append variable inside env
             - Else,  add new variable into the env
 Return: None
 */
-int ft_export(char **arg, t_shell *data)
+int	ft_export(char **arg, t_shell *data)
 {
-    int i;
-    int track;
+	int	i;
+	int	track;
 
-    (i = 0, track = 0);
-    if (arg[1] == NULL)
-    {
-        print_sorted_env(data->env);
-        return (EXIT_SUCCESS);
-    }
-    while (arg[++i])
-    {
-        if (!process_var(arg[i], data->env, &track) && ++track)
-            err_msg_1("export", arg[i], "fail to add into env");
-    }
-    if (track)
-        return (EXIT_FAILURE);
-    return (EXIT_SUCCESS);
+	i = 0;
+	track = 0;
+	if (arg[1] == NULL)
+	{
+		print_sorted_env(data->env);
+		return (EXIT_SUCCESS);
+	}
+	while (arg[++i])
+	{
+		if (!process_var(arg[i], data->env, &track) && ++track)
+			err_msg_1("export", arg[i], "fail to add into env");
+	}
+	if (track)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
 
-static int process_var(char *s, t_env *env, int *track)
+static int	process_var(char *s, t_env *env, int *track)
 {
-    int i;
-    i = 0;
-    if (!is_identifier(s, &i) && ++(*track))
-        err_msg_1("export", s, "not a valid identifier");
-    else if (s[i] == '=')
-    {
-        s[i] = '\0';
-        if (env_val(s, env))    return (update_env(s, &s[i + 1], env));
-        else                    return (append_env(s, &s[i + 1], env));
-    }
-    return (TRUE);
+	int	i;
+
+	i = 0;
+	if (!is_identifier(s, &i) && ++(*track))
+		err_msg_1("export", s, "not a valid identifier");
+	else if (s[i] == '=')
+	{
+		s[i] = '\0';
+		if (env_val(s, env))
+			return (update_env(s, &s[i + 1], env));
+		else
+			return (append_env(s, &s[i + 1], env));
+	}
+	return (TRUE);
 }
 
-static void print_sorted_env(t_env *env)
+static void	print_sorted_env(t_env *env)
 {
-    int i;
-    char **list;
+	int		i;
+	char	**list;
 
-    i = -1;
-    list = env_key_list(env);
-    if (!list)
-        return ;
-    sort_list(list);
-    while (list[++i])
-        printf("declare -x %s=\"%s\"\n", list[i], env_val(list[i], env));
-    free(list);
+	i = -1;
+	list = env_key_list(env);
+	if (!list)
+		return ;
+	sort_list(list);
+	while (list[++i])
+		printf("declare -x %s=\"%s\"\n", list[i], env_val(list[i], env));
+	free(list);
 }
 
-static char **env_key_list(t_env *env)
+static char	**env_key_list(t_env *env)
 {
-    int i;
-    char **list;
-    
-    i = 0;
-    list = (char **)malloc(sizeof(char *) * (env_len(env) + 1));
-    if (!list)
-        return (NULL);
-    while (env)
-    {
-        list[i++] = env->key;   
-        env = env->next;
-    }
-    list[i] = NULL;
-    return (list);
+	int		i;
+	char	**list;
+
+	i = 0;
+	list = (char **)malloc(sizeof(char *) * (env_len(env) + 1));
+	if (!list)
+		return (NULL);
+	while (env)
+	{
+		list[i++] = env->key;
+		env = env->next;
+	}
+	list[i] = NULL;
+	return (list);
 }
 
-static void sort_list(char **list)
+static void	sort_list(char **list)
 {
-    char *temp;
-    int i;
-    int len;
+	char	*temp;
+	int		i;
+	int		len;
 
-    (i = 1 , len = 0);
-    while (list && list[len])
-        len++;
-    while (--len)
-    {
-        i = -1;
-        while (++i < len)
-        {
-            if (ft_strcmp(list[i], list[i + 1]) > 0)
-            {
-                temp = list[i + 1];
-                list[i + 1] = list[i];
-                list[i] = temp;
-            }
-        }
-    }
+	i = 1;
+	len = 0;
+	while (list && list[len])
+		len++;
+	while (--len)
+	{
+		i = -1;
+		while (++i < len)
+		{
+			if (ft_strcmp(list[i], list[i + 1]) > 0)
+			{
+				temp = list[i + 1];
+				list[i + 1] = list[i];
+				list[i] = temp;
+			}
+		}
+	}
 }
--- built_in/unset.c
+++ built_in/unset.c
@@ -12,100 +12,100 @@
 
 #include "../include/minishell.h"
 
-void free_env_node(t_env *env);
+void	free_env_node(t_env *env);
 
 /* Test
 int main()
 {
-    t_shell data;
+	t_shell data;
 
-    t_env *n1 = (t_env *)malloc(sizeof(t_env));
-    t_env *n2 = (t_env *)malloc(sizeof(t_env)); 
-    t_env *n3 = (t_env *)malloc(sizeof(t_env));
-    t_env *n4 = (t_env *)malloc(sizeof(t_env)); 
-    t_env *n5 = (t_env *)malloc(sizeof(t_env));
+	t_env *n1 = (t_env *)malloc(sizeof(t_env));
+	t_env *n2 = (t_env *)malloc(sizeof(t_env)); 
+	t_env *n3 = (t_env *)malloc(sizeof(t_env));
+	t_env *n4 = (t_env *)malloc(sizeof(t_env)); 
+	t_env *n5 = (t_env *)malloc(sizeof(t_env));
 
-    n1->key = strdup("HOME");
-    n1->val = strdup("/home/jpaul");
-    n1->next = n2;
+	n1->key = strdup("HOME");
+	n1->val = strdup("/home/jpaul");
+	n1->next = n2;
 
-    n2->key = strdup("USER");
-    n2->val = strdup("Jason");
-    n2->next = n3;
+	n2->key = strdup("USER");
+	n2->val = strdup("Jason");
+	n2->next = n3;
 
-    n3->key = strdup("SHELL");
-    n3->val = strdup("/bash/");
-    n3->next = n4;
+	n3->key = strdup("SHELL");
+	n3->val = strdup("/bash/");
+	n3->next = n4;
 
-    n4->key = strdup("SHLVL");
-    n4->val = strdup("1");
-    n4->next = n5;
+	n4->key = strdup("SHLVL");
+	n4->val = strdup("1");
+	n4->next = n5;
 
-    n5->key = strdup("PWD");
-    n5->val = strdup("/home/usr/Desktop");
-    n5->next = NULL;
+	n5->key = strdup("PWD");
+	n5->val = strdup("/home/usr/Desktop");
+	n5->next = NULL;
 
-    data.env = n1;
-    // char *a0[] = {"unset", NULL};
-    // char *a1[] = {"unset", "SHELL=", NULL};
-    // char *a2[] = {"unset", "3413423", "ewqeeq", NULL};
+	data.env = n1;
+	// char *a0[] = {"unset", NULL};
+	// char *a1[] = {"unset", "SHELL=", NULL};
+	// char *a2[] = {"unset", "3413423", "ewqeeq", NULL};
 
-    char *a4[] = {"unset", "HOME", "SHELL", "3343242", NULL};
-    // char *a5[] = {"unset", "SHELL", NULL};
-    //char *a6[] = {"unset", "PWD", NULL};
-    
-    t_env *current = data.env;
-    while (current)
-    {
-        printf("%s=%s\n", current->key, current->val);
-        current = current->next;
-    }
-    printf("\n======================\n");
-    ft_unset(a4, &data);
-    current = data.env;
-    while (current)
-    {
-        printf("%s=%s\n", current->key, current->val);
-        current = current->next;
-    }
-    destroy_env(&data);
+	char *a4[] = {"unset", "HOME", "SHELL", "3343242", NULL};
+	// char *a5[] = {"unset", "SHELL", NULL};
+	//char *a6[] = {"unset", "PWD", NULL};
+	
+	t_env *current = data.env;
+	while (current)
+	{
+		printf("%s=%s\n", current->key, current->val);
+		current = current->next;
+	}
+	printf("\n======================\n");
+	ft_unset(a4, &data);
+	current = data.env;
+	while (current)
+	{
+		printf("%s=%s\n", current->key, current->val);
+		current = current->next;
+	}
+	destroy_env(&data);
 }*/
 
 /* unset
 Description: Remove var from env listed-list
 */
-int ft_unset(char **arg, t_shell *data)
+int	ft_unset(char **arg, t_shell *data)
 {
-    int i;
-    t_env   *current;
-    t_env   *previous;
+	int		i;
+	t_env	*current;
+	t_env	*previous;
 
-    i = 0;
-    while (arg[++i])
-    {
-        current = data->env;
-        previous = NULL;
-        while (current)
-        {
-            if (!ft_strcmp(arg[i], current->key))
-            { 
-                if (previous)
-                    previous->next = current->next;
-                else
-                    data->env = current->next;
-                free_env_node(current);
-                break;
-            }
-            previous = current;
-            current = current->next;
-        }
-    }
-    return (EXIT_SUCCESS);
+	i = 0;
+	while (arg[++i])
+	{
+		current = data->env;
+		previous = NULL;
+		while (current)
+		{
+			if (!ft_strcmp(arg[i], current->key))
+			{
+				if (previous)
+					previous->next = current->next;
+				else
+					data->env = current->next;
+				free_env_node(current);
+				break ;
+			}
+			previous = current;
+			current = current->next;
+		}
+	}
+	return (EXIT_SUCCESS);
 }
 
-void free_env_node(t_env *env)
+void	free_env_node(t_env *env)
 {
-    free(env->key);
-    free(env->val);
-    free(env);
+	free(env->key);
+	free(env->val);
+	free(env);
 }
